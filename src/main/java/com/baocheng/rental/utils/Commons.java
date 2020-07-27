4b18a88ea94ec715f00c59341a783e841e82bf29
ordili <lijidong@ict.ac.cn>
2020-07-27 16:53:46 +0800

    delete extra import

--- src/main/java/com/baocheng/rental/utils/Commons.java
+++ src/main/java/com/baocheng/rental/utils/Commons.java
@@ -1,7 +1,5 @@
 package com.baocheng.rental.utils;
 
-import com.sun.org.apache.bcel.internal.generic.PUSH;
-
 public class Commons {
 
     public static final String GET_ORDER_QUANTITY_METHOD_NAME = "getOrderQuantity";
